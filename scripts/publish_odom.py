c3044a7c8a0b980dcef5fdebb952c8b54172b3b5
Dan Lazewatsky <dan@lazewatsky.com>
2014-08-21 15:24:43 -0500

    add tf

--- scripts/publish_odom.py
+++ scripts/publish_odom.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env rosh
+import tf
+
+br = tf.TransformBroadcaster()
 
 for model_state in topics.gazebo.model_states[:]:
     for name, pose, twist in zip(model_state.name, model_state.pose, model_state.twist):
@@ -6,7 +9,14 @@ for model_state in topics.gazebo.model_states[:]:
             pose=msg.geometry_msgs.PoseWithCovariance(pose=pose),
             twist=msg.geometry_msgs.TwistWithCovariance(twist=twist)
         )
-        odom.child_frame_id = 'base_footprint'
+        odom.child_frame_id = name
         odom.header.frame_id = '/odom'
         odom.header.stamp = now()
         topics['/%s/odom' % name](odom)
+        br.sendTransform(
+            (pose.position.x, pose.position.y, pose.position.z),
+            (pose.orientation.x, pose.orientation.y, pose.orientation.z, pose.orientation.w),
+            now(),
+            name,
+            '/odom'
+        )
